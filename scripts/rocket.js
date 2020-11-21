2aac27224061e47a7f9e39c9f8fb023bcb0257fc
Zoe Zhang <whereiszz@gmail.com>
2020-11-21 19:39:36 +1100

    final details with drone designs

--- scripts/rocket.js
+++ scripts/rocket.js
@@ -1,6 +1,9 @@
 var timer = null;
 var countdownNumber = 10;
 
+// setTimeout(() => {
+//     location.href = "https://www.google.com";
+// }, 2000);
 
 
 var changeStage = function (stage) {
@@ -8,11 +11,28 @@ var changeStage = function (stage) {
     clearInterval(timer);
     countdownNumber = 10;
     document.getElementById("countdown").innerText = countdownNumber;
+    document.getElementById("nervous").className = "nervous";
+    document.getElementById("cant-wait").className = "cant-wait";
     
     if (stage == 2) {
         timer = setInterval(() => {
             countdownNumber = countdownNumber - 1;
             document.getElementById("countdown").innerText = countdownNumber;
+            
+            if (countdownNumber <= 7 && countdownNumber> 4) {
+                //be nervous
+                document.getElementById("nervous").className = "nervous show";
+            } else {
+                document.getElementById("nervous").className = "nervous";
+            }
+
+            if (countdownNumber <= 4 && countdownNumber> 1) {
+                //cannot wait
+                document.getElementById("cant-wait").className = "cant-wait show";
+            } else {
+                document.getElementById("cant-wait").className = "cant-wait";
+            }
+
             if (countdownNumber <= 0) {
                 changeStage(3);
             };
@@ -21,7 +41,7 @@ var changeStage = function (stage) {
         var success = setTimeout(() => {
             var randomNumber = Math.round(Math.random()*10);
             console.log("randomNumber: "+randomNumber)
-            
+        
             if (randomNumber >= 5) {
                 changeStage(4); //success
             } else {
